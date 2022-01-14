e15a3ca20e772193335fa30637616644c9cb90a0
Dmitrij Artjukh <dmitrij.artjukh@privatbank.ua>
2022-01-14 17:35:52 +0200

    14 commit

--- lesson2/14.js
+++ lesson2/14.js
@@ -1,13 +1,12 @@
-let n=1000
-let num=0
-while(true)
-{
-    if ((n/2)>50) {
-        n=n/2
-        num++ 
-    } 
+let n = 1000
+let num = 0
+while (true) {
+    if ((n / 2) > 50) {
+        n = n / 2
+        num++
+    }
     else break
 }
 
-console.log('Результат: ',n)
-console.log('Всего итареций: ',num)
+console.log('Результат: ', n)
+console.log('Всего итареций: ', num)
