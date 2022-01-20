c20ec1b865c1615b3b9cacaec1d505ddfdd0fae6
Dmitrij Artjukh <dmitrij.artjukh@privatbank.ua>
2022-01-20 17:58:21 +0200

    commit lesson4

---
+++ lesson3/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+function f(tmp) {
+//     if (i < 3) {
+
+//         console.log(tmp.shift)
+//         i++
+//         f(rem)
+//     }
+//     //return f(rem)
+// }
+// console.log(f([1, 2, 3]));
