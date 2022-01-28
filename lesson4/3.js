ec051c716d92d9411746c4acf5ff810b8495e883
Dmitrij Artjukh <dmirij.artjukh@privatbank.ua>
2022-01-28 11:43:22 +0200

    3change  commit

--- lesson4/3.js
+++ lesson4/3.js
@@ -8,20 +8,40 @@
 // - Второй параметр обязателен и может принимать только функцию
 
 // ```javascript
- const arr = [1,-2,3];
- let i=0
+ const arr = [1,2,3];
+ let l=0;
 // every(arr, function(item, i, arr) {});
 // ```
-const every=function (arr, callback) {
-    if (typeof(callback)=='function'){
-          if (Array.isArray(arr)){
-             for (i = 0; i < arr.length; i ++) {
-                    callback(arr[i], i, arr);
-               }
-         } else throw new Error("not a array")
-     }else throw new Error ("not a function")
-   };
+// const every =  function (arr, callback) {
+//     if (typeof(callback)=='function'){
+//           if (Array.isArray(arr)){
+//              for (i = 0; i < arr.length; i ++) {
+//               callback(arr[i], i, arr);
+//               if (arr[i]<0){
+//                  return false
+//                } 
+//               }                   
+//               return true     
+//            } else throw new Error("not a array")
+//      }else throw new Error ("not a function")
+//    };
 
- console.log(  every(arr, function(item, i, arr) {
-}));
+//  console.log (every(arr, function(item, i, arr) {
    
+//   }));
+ 
+function every(arr, callback){
+  if (typeof(callback)=='function'){
+               if (Array.isArray(arr)){
+                  for (i = 0; i < arr.length; i ++) {
+                       if (callback(arr[i], i, arr)){return true}   
+                  }     
+                } else throw new Error("not a array")
+          }else throw new Error ("not a function")
+         return false
+        };
+     
+console.log (every(arr, function(item, i, arr) {
+                 item<0
+             }));
+           
